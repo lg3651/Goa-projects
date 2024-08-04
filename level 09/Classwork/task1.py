bd4c0ee4e38675821569147a9d05e4a8274361e0
lg3651 <lguarnera712@gmail.com`>
2024-08-04 15:25:10 +0400

    commit

---
+++ level 09/Classwork/task1.py
@@ -0,0 +1,20 @@
+'''1) # ლოგიკური ოპერატორების გამოყენებით შექმენით ყველა შესაძლო ვარიანტის ოპერაცია and ზეც და or ზეც, 
+შეასრულოთ ლოგიკური ოპერაციები შედარების ოპერატორებსი გამოყენებით(<, >) ყველა შესაძლო ვარიანტი'''
+
+#and logical operator
+
+a=False
+b=False
+print(a and b)
+print(False and True)
+print(3>2 and 5<2)
+print(4==4 and 5!=7)
+
+#or logical operator
+
+a=4>7
+b=24>54
+print(a or b)
+print(False or True)
+print(True or False)
+print(4+5==9 or 6-2==4)
---
+++ level 10/Homework/task2.py
@@ -0,0 +1,2 @@
+'''2) გადახედეთ https://www.w3schools.com/python/ref_random_choice.asp'''
+'''გადავხედე'''
